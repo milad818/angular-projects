0261d94997370646883ce8d2a1406c9557c39a7a
Milad Zakhireh <eng.zakhireh@gmail.com>
2025-11-23 22:29:21 +0100

    Explore signals, property binding, event binding, and type assertion

--- angular-intro/src/app/app.ts
+++ angular-intro/src/app/app.ts
@@ -12,4 +12,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-intro');
+
+  // Variables in classes are not signals by default
+  // To turn them into signal so that the changes can be tracked, we do as follows:
+  name = signal('Luis')
+  // you can also pic different images from picsum.photos/images
+  imageURL = signal("https://picsum.photos/id/10/200/200")
+
+  getName() {
+    return this.name();
+  }
+
+
+  // This will raise an error
+  // since TypeScript isn't aware there's a property (value) on the target object
+  // however it does exist on the target object
+  changeImage(e: KeyboardEvent) {
+    // below type assertion is applied; otherwise, throws an error
+    // that is, type is asserted before accessing the value
+    this.imageURL.set((e.target as HTMLInputElement).value);
+  }
+
+  changeImageButton(url: string) {
+    this.imageURL.set(url);
+  }
+
 }
