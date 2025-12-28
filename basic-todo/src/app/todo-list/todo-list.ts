6b5e769b61c8dd09ffa8aca06ee70c0af8d1be6a
Milad Zakhireh <eng.zakhireh@gmail.com>
2025-12-28 23:27:39 +0330

    Complete basic-todo project: add html, styles, and error handling feature

--- basic-todo/src/app/todo-list/todo-list.ts
+++ basic-todo/src/app/todo-list/todo-list.ts
@@ -11,14 +11,25 @@ import { FormsModule } from '@angular/forms';
 export class TodoList {
 
   tasks: string[] = [];
-
   newTask: string = '';
+  errorMessage: string | null = null;
 
   addTask() {
+
+    // Validation
+    if (!this.newTask.trim()) {
+      this.errorMessage = 'Task cannot be empty!';
+      this.newTask = "";
+      return;
+    }
+
     this.tasks.push(this.newTask);
     this.newTask = ''
+    this.errorMessage = null;
   }
 
+  // Receives the start index and 1 as remove count
+  // Therefore, removes the item corresponding to the index
   removeTask(index: number) {
     this.tasks.splice(index, 1);
   }
