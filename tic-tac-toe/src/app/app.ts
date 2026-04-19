c5972c92085aaeb6b6644147d2bc12fce8f06d38
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-04-19 18:16:19 +0200

    Create tic-toc-toe component and apply the initial config

--- tic-tac-toe/src/app/app.ts
+++ tic-tac-toe/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TicTacToe } from './tic-tac-toe/tic-tac-toe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TicTacToe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('tic-tac-toe');
-}
+export class App {}
