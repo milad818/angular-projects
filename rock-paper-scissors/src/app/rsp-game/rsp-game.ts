fe1be3ad05da7d90d39898e3e5fbc0e487a2a7df
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-04-21 20:42:39 +0200

    Complete Rock-Paper-Scissors project: - Implement logic and all the necessary methods - Implement HTML - Add styles

--- rock-paper-scissors/src/app/rsp-game/rsp-game.ts
+++ rock-paper-scissors/src/app/rsp-game/rsp-game.ts
@@ -8,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class RspGame {
 
+  // A more generic typing approach for arrays
+  // choice: Array<string> = ["Rock","Paper","Scissors"];
+
+  choices: string[] = ["Rock","Paper","Scissors"];
+  playerChoice: string | null = null;
+  computerChoice: string | null = null;
+  result: string | null = null;
+
+  // random() returns a decimal between 0 and 1
+  getRandomNumber(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  // APPROACH A
+  determineWinner(player: string, computer: string): string {
+    if (player === computer) return "No winner, it is a tie!";
+    if (
+      player === "Rock" && computer === "Scissors" ||
+      player === "Scissors" && computer === "Paper" ||
+      player === "Paper" && computer === "Rock"
+    ) {
+      return "You won!";
+    }
+    return "You lost!";
+  }
+
+  play(choice: string) {
+    this.playerChoice = choice;
+    this.computerChoice = this.choices[(this.getRandomNumber(this.choices.length))]
+    this.result = this.determineWinner(this.playerChoice, this.computerChoice)
+  }
 }
+
