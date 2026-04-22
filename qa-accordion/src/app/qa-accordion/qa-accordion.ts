c3986da5bc45634bd40e206f30677ece2f798fd2
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-04-22 19:16:57 +0200

    Implement the logic and the HTML structure

--- qa-accordion/src/app/qa-accordion/qa-accordion.ts
+++ qa-accordion/src/app/qa-accordion/qa-accordion.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+
+interface QA {
+  question: string,
+  answer: string
+}
+
 @Component({
   selector: 'app-qa-accordion',
   imports: [CommonModule],
@@ -8,5 +14,27 @@ import { Component } from '@angular/core';
   styleUrl: './qa-accordion.css',
 })
 export class QaAccordion {
+  openedIndex: number | null = null;
 
+  qas: QA[] = [
+      {
+        question: 'What is Angular?',
+        answer: 'Angular is a platform for building mobile and desktop web applications.',
+      },
+
+      {
+        question: 'What is a component in Angular?',
+        answer:'A component controls a patch of the screen called a view. Components are the main building block of Angular applications.',
+      },
+
+      {
+        question: 'What are Angular directives?',
+        answer: 'Directives are instructions in the DOM. Angular directives allow you to attach behavior to elements in the DOM.',
+      },
+    ]
+
+  toggleQA(index: number) {
+    // Pay attention to strict equality operator, where the condition is checked
+    this.openedIndex = this.openedIndex === index ? null : index;
+  }
 }
