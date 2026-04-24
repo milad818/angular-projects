a449230f4d54cca37861d162e57e28ef8d08def2
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-04-24 22:17:56 +0200

    Implement random joke service

--- rand-joke-gen/src/app/services/joke.ts
+++ rand-joke-gen/src/app/services/joke.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+interface RandomJoke {
+  setup: string,
+  punchline: string
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class Joke {
-  
+
+  private apiURL = 'http://official-joke-api.appspot.com/jokes/random';
+
+  private http = inject(HttpClient);    // Modern alternative to contructor(private http: HttpClient)
+
+  getRandomJoke(): Observable<RandomJoke> {
+    return this.http.get<RandomJoke>(this.apiURL);
+  }
+
 }
