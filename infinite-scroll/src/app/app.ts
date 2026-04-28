468ae9d9d3b9bb00c55e9d93799d2afc59263512
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-04-28 17:21:14 +0200

    Create Post and Post List components, and apply the initial config

--- infinite-scroll/src/app/app.ts
+++ infinite-scroll/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PostList } from './components/post-list/post-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PostList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
