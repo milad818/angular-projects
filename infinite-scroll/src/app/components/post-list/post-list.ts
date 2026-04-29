8f18e56dbcade96dcf34a7b3d38702a2a2af9ca5
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-04-29 11:50:26 +0200

    - Implement post list component - Add proper error handling in post service - Update README.md with information on JSON Server

--- infinite-scroll/src/app/components/post-list/post-list.ts
+++ infinite-scroll/src/app/components/post-list/post-list.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Inject, inject, OnInit } from '@angular/core';
+import { Post } from '../post/post';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post-list',
-  imports: [],
+  imports: [CommonModule, Post],
   templateUrl: './post-list.html',
   styleUrl: './post-list.css',
 })
-export class PostList {
+export class PostList implements OnInit {
+  posts: JSONPlaceholderPost[] = [];
+  isLoading = false;
+  pageNumber: number = 1;
+  itemLimit: number = 10;
+  errorMessage: string = '';
+
+  postService = Inject(PostService);
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  private handleError(error: any): void {
+    console.error('Error raised while fetching posts:', error);
+    this.errorMessage = 'No able to fetch posts at the moment, please try again later!'
+  }
+
+  loadPosts(): void {
+    this.isLoading = true;
+    this.postService.getPosts(this.pageNumber, this.itemLimit).subscribe({
+      next: (newPosts: any[]) => {
+        if (newPosts && newPosts.length > 0) {
+          // Append new posts to the existing list of posts
+          this.posts = [...this.posts, ...newPosts];
+          this.pageNumber++;
+        }
+      },
+      error: (error: any) => {
+        this.handleError(error);
+      },
+      complete: () => {
+        this.isLoading = false;
+      }
+    });
+  }
+
+  // APPROACH A: Legacy Approach
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    // 1. Calculate how much has been scrolled
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    const max = document.documentElement.scrollHeight;
+
+    // 2. Check if we are near the bottom (e.g., 200px threshold)
+    if (pos >= max - 200 && !this.isLoading) {
+      this.loadPosts();
+    }
+  }
+
+  // APPROACH B: Intersection Observer
+  // TO-DO
 
 }
--- infinite-scroll/src/app/services/post.service.ts
+++ infinite-scroll/src/app/services/post.service.ts
@@ -1,22 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Post {
+export class PostService {
   private readonly API_URL = 'https://jsonplaceholder.typicode.com/posts'
 
   private http = inject(HttpClient);
 
   // APPROACH A: Old-fashioned
-  getPosts(pageNumber: number, limit:number): Observable<any[]> {
+  // Fetch a certain number of pages and number of items per page
+  getPosts(pageNumber: number, itemLimit:number): Observable<any[]> {
     return this.http.get<any[]>(
       // Everything before the ? tells the browser where to go (e.g., the posts resource on your server).
       // : Everything after the ? is data being passed to the server to filter or modify the result.
-      `${this.API_URL}?_pages=${pageNumber}&_limit=${limit}`
-    );
+      // All based on advanced querying supported by JSON Server
+      `${this.API_URL}?_pages=${pageNumber}&_limit=${itemLimit}`)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(() => new Error('Something went wrong while fetching posts!'))
+        })
+      );
   }
 
+  // APPPROACH B: More modern and sophisticated
+  // TO-DO
 }
