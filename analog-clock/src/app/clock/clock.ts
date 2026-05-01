91d8e770e0d80b849a614609bc89d4df75d0fed0
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-05-01 12:32:56 +0200

    Create Clock component, services, and apply the initial config

---
+++ analog-clock/src/app/clock/clock.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  imports: [],
+  templateUrl: './clock.html',
+  styleUrl: './clock.css',
+})
+export class Clock {
+
+}
