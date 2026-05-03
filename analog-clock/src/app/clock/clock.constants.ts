06f20a1264d478316162bb0853b521b2646379d5
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-05-03 21:03:52 +0200

    - Update logic, syles and structure: - logic with clock update method - clock styles and HTML structure with hands - constants with new values

--- analog-clock/src/app/clock/clock.constants.ts
+++ analog-clock/src/app/clock/clock.constants.ts
@@ -1,7 +1,14 @@
 
 export const CLOCK_CONSTANTS = {
+  // NUMBERS
   CENTER_OFFSET: 50,      // Center relative to the numbers
   RADIUS: 45,             // Distance of numbers form the center
-  DEGREE_PER_HOUR: 30,    // Degrees moved by the hour hand per hour
-  DEGREE_TO_RADIAN: Math.PI / 180   // Convert degrees to radian
+  DEGREES_PER_HOUR: 30,    // Degrees moved by the hour hand per hour
+  DEGREES_TO_RADIAN: Math.PI / 180,   // Convert degrees to radian
+
+  // HANDS
+  DEGREES_PER_MIN_OR_SEC: 6,    // Degrees to move per minute or second
+  MINUTE_ADJUSTMENT: 0.5,       // Hour hand adjustment per minute
+  SECOND_ADJUSTMENT: 0.1,       // Minute hand adjustment per second
+  OFFSET_ROTATION: 180          // Rotationoffsetfor alignment
 };
--- analog-clock/src/app/clock/clock.ts
+++ analog-clock/src/app/clock/clock.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { CLOCK_CONSTANTS as cc } from './clock.constants';
+import { TimeService } from '../services/time.service'
 
 @Component({
   selector: 'app-clock',
@@ -8,13 +9,16 @@ import { CLOCK_CONSTANTS as cc } from './clock.constants';
   templateUrl: './clock.html',
   styleUrl: './clock.css',
 })
-export class Clock {
+export class Clock implements OnInit {
 
   // Rotation angles
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
   clockNumbers = this.generateClockNumbers();
+
+  private timeService = inject(TimeService)
+  private cdr = inject(ChangeDetectorRef);
 
   generateClockNumbers(): ClockNumber[] {
 
@@ -24,7 +28,7 @@ export class Clock {
 
     // Loop through clock numbers (1-12) to calculate their positions
     for (let n = 1; n <= 12; n++) {
-      const angle = (n - 3) * cc.DEGREE_PER_HOUR * cc.DEGREE_TO_RADIAN
+      const angle = (n - 3) * cc.DEGREES_PER_HOUR * cc.DEGREES_TO_RADIAN
       const top = centerOffset + radius * Math.sin(angle);
       const left = centerOffset + radius * Math.cos(angle);
 
@@ -39,5 +43,25 @@ export class Clock {
     }
 
     return numbers;
+  }
+
+  ngOnInit() {
+    this.updateClock();
+    setInterval(() => this.updateClock(), 1000);
+  }
+
+  // Update hands of the clock
+  updateClock() {
+    const now = this.timeService.getCurrentTime();
+    this.hours = (now.getHours() % 12) * cc.DEGREES_PER_HOUR +
+                  now.getMinutes() * cc.MINUTE_ADJUSTMENT +
+                  cc.OFFSET_ROTATION;
+    this.minutes = now.getMinutes() * cc.DEGREES_PER_MIN_OR_SEC +
+                  now.getSeconds() * cc.SECOND_ADJUSTMENT +
+                  cc.OFFSET_ROTATION;
+    this.seconds = now.getSeconds() * cc.DEGREES_PER_MIN_OR_SEC +
+                  cc.OFFSET_ROTATION;
+
+    this.cdr.markForCheck();
   }
 }
