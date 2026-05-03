42fd2f4d4087f9137d92ae5915ba82fd1c0c0cb2
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-05-03 21:03:36 +0200

    Implement time servide to get current time

--- analog-clock/src/app/services/time.service.ts
+++ analog-clock/src/app/services/time.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Time {
-  
+export class TimeService {
+  getCurrentTime(): Date {
+    return new Date();
+  }
 }
