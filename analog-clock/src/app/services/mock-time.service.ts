4b8c4ecafc3b9850112d2c01182c1e55d2a38504
Milad Zakhireh <eng.zakhireh@gmail.com>
2026-09-05 11:49:31 +0200

    Revise comment for mock data in Analog Clock

--- analog-clock/src/app/services/mock-time.service.ts
+++ analog-clock/src/app/services/mock-time.service.ts
@@ -8,7 +8,8 @@ export class MockTimeService {
   private mockDate: Date = new Date();
 
   getCurrentTime(): Date {
-    this.mockDate.setHours(5);
+    this.mockDate.setHours(5); // Only hour set to 5 as default and both min and sec are taken from the system
+                               // You can also set a value for all as default: .. .setHours(5, 30, 10)
     return this.mockDate;   // Avoid "return this.mockDate.setHours(5)", it returns the result produced by the method
   }
 
